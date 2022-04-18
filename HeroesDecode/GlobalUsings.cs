6ade2f247e9d429fb2e1805b4cf8aee15f19ea9d
Kevin Oliva <kevin.k.oliva@gmail.com>
2022-04-18 18:26:55 -0500

    Updated commandline package

---
+++ HeroesDecode/GlobalUsings.cs
@@ -0,0 +1,6 @@
+﻿global using Heroes.StormReplayParser;
+global using Heroes.StormReplayParser.Player;
+global using Heroes.StormReplayParser.Replay;
+global using HeroesDecode;
+global using System.CommandLine;
+global using System.Text;
